5d9f92f2b4f917d2c7ffa72ef07cd75890adf4c3
Nikita <silvernikchuprov@gmail.com>
2022-10-13 09:50:38 +0300

    Task23 done

---
+++ Task23/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+
+while (count <= num)
+{
+    Console.WriteLine(Square(count));
+    count++;
+}
+string Square(int number)
+{
+    return ($"{number} -> {number*number*number}");
+}
